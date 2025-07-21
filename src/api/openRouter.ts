8e97591f96d7b4335a84e4eaa20a74676da4f965
Enoxlaalu <enoxlaalu@gmail.com>
2025-07-21 11:44:30 +0300

    moved secret key to .env file

--- src/api/openRouter.ts
+++ src/api/openRouter.ts
@@ -1,10 +1,8 @@
-import { OPEN_ROUTER_KEY } from 'src/constants/constants'
-
 export const makeGptSearch = async (query: string) => {
   const res = await fetch('https://openrouter.ai/api/v1/chat/completions', {
     method: 'POST',
     headers: {
-      Authorization: `Bearer ${OPEN_ROUTER_KEY}`,
+      Authorization: `Bearer ${import.meta.env.VITE_OPEN_ROUTER_KEY}`,
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
